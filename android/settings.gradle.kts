8667acb62671bea5d8383650258e504f93c52551
Sanchit Batra <sanchitbatra@Sanchits-MacBook-Air.local>
2026-04-30 21:40:46 +0530

    Completing firebase setup for android -- IOS Pending

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,10 +19,13 @@ pluginManagement {
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
 
-    // ✅ FIXED VERSIONS
+    // Android & Kotlin
     id("com.android.application") version "8.9.1" apply false
     id("com.android.library") version "8.9.1" apply false
     id("org.jetbrains.kotlin.android") version "2.1.0" apply false
+
+    // ✅ Firebase Google Services Plugin
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 include(":app")
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -17,6 +18,9 @@ void main() async {
 
   final localeProvider = LocaleProvider();
   await localeProvider.loadLocale();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   runApp(
   MultiProvider(
